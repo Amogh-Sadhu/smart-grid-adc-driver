898ff49fc83313f252e4b90a86d39b3d265d1b5f
Amogh-Sadhu <amoghsadhu27@gmail.com>
2026-04-23 16:52:50 +0530

    basic changes done

--- smart_grid_adc.c
+++ smart_grid_adc.c
@@ -4,27 +4,50 @@
 
 #include <linux/iio/iio.h>
 
-MODULE_LICENSE("GPL");
+struct bbAdc_state{
 
-struct ad7292_state {
-	struct spi_device *spi;
 };
 
-static int ad7292_setup(struct ad7292_state *st)
+static int bbAdc_setup(struct ad7292_state *st)
 {
 	return 0;
 }
  
-static int ad7292_read_raw(struct iio_dev *indio_dev,
+static int bbAdc_read_raw(struct iio_dev *indio_dev,
 			   const struct iio_chan_spec *chan,
 			   int *val, int *val2, long info)
 {
 	return 0;
 }
 
-static int ad7292_write_raw(struct iio_dev *indio_dev,
+static int bbAdc_write_raw(struct iio_dev *indio_dev,
 			    struct iio_chan_spec const *chan,
 			    int val, int val2, long info)
 {
 	return 0;
 }
+
+static int bbAdc_probe(struct spi_device *spi){
+	
+}
+
+static int bbAdc_remove( struct spi_device *spi){
+
+}
+
+static const struct iio_sw_device_ops iio_dummy_device_ops = {
+	.probe = bbAdc_probe,
+	.remove = bbAdc_remove,
+};
+
+static struct iio_sw_device_type iio_dummy_device = {
+	.name = "smart-grid-adc",
+	.owner = THIS_MODULE,
+	.ops = &iio_dummy_device_ops,
+};
+
+module_iio_sw_device_driver(iio_dummy_device);
+
+MODULE_AUTHOR("Amogh Sadhu <amoghsadhu27@gmail.com>");
+MODULE_DESCRIPTION("ADC driver for beaglebone black");
+MODULE_LICENSE("GPL v3");
